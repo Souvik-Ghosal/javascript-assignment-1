833cad3d6a79b658f6e78dbb7ceffc331be41741
Souvik-Ghosal <62163357+Souvik-Ghosal@users.noreply.github.com>
2022-01-28 13:10:59 +0530

    Add files via upload

---
+++ 3swap.js
@@ -0,0 +1,11 @@
+let a = 10 , b = 20;
+console.log("before swapping")
+console.log("a = "+a)
+console.log("b = "+b)
+
+let c = a;
+a=b;
+b=c;
+console.log("after swapping")
+console.log("a = " + a);
+console.log("b = "+b);
