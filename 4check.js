5674e35c6f6f3078fd08e18fbadaff2f85ca031e
Souvik-Ghosal <62163357+Souvik-Ghosal@users.noreply.github.com>
2022-01-28 13:18:32 +0530

    Add files via upload

---
+++ 4check.js
@@ -0,0 +1,11 @@
+let number = prompt("enter a number");
+
+if(number == 0)
+  console.log("Number is zero");
+
+  if(number > 0)
+  console.log("Number is positive");
+
+  if(number < 0)
+  console.log("Number is negative");
+
